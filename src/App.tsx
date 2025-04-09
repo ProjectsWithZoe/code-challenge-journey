6874328ac8effa7aa09a97e75e3ca4886d66845a
Zoe Mwangi <your-email@example.com>
2025-04-09 16:11:37 +0100

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Welcome } from "./components/Welcome";
+import { WelcomePage } from "./components/Welcome";
 
 const queryClient = new QueryClient();
 
@@ -18,7 +18,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/welcome" element={<WelcomePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
