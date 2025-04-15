3ba157e1858484f9fb410f940be03baac398ca03
Zoe Mwangi <your-email@example.com>
2025-04-15 14:56:22 +0100

    serviceworker removed

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { VitePWA } from "vite-plugin-pwa";
+//import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,7 +11,8 @@ export default defineConfig(({ mode }) => ({
     //port: 8080,
   },
   plugins: [
-    VitePWA({
+    ,
+    /*VitePWA({
       registerType: "autoUpdate",
       manifest: {
         name: "Daily Code Challenge",
@@ -34,8 +35,7 @@ export default defineConfig(({ mode }) => ({
           },
         ],
       },
-    }),
-    react(),
+    })*/ react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
